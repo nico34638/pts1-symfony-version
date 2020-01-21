4ffa3e20bef434d0a66facfaf2c7895013bea58a
Nicolas Fidel <nico24093@gmail.com>
2020-01-21 09:32:11 +0100

    add delete article

--- src/Controller/Admin/AdminArticleController.php
+++ src/Controller/Admin/AdminArticleController.php
@@ -6,20 +6,61 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
-
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 /**
  * @Route("/admin/article")
  */
 class AdminArticleController extends AbstractController
-{   
+{
+    /**
+     *@var ArticleRepository
+     */
+    private $articleRepository;
+
+    /**
+     *@var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(ArticleRepository $articleRepository, EntityManagerInterface $em)
+    {
+        $this->articleRepository = $articleRepository;
+        $this->em = $em;
+    }
+
     /**
      * @Route("/", name="admin.article.index", methods="GET")
      */
-    public function index(ArticleRepository $articleRepository)
+    public function index()
     {
         return $this->render('admin/article/index.html.twig', [
             'controller_name' => 'AdminArticleController',
-            'articles' => $articleRepository->findAllArticle()
+            'articles' => $this->articleRepository->findAllArticle()
         ]);
     }
+
+    /**
+     * @Route("/edit", name="admin.article.edit", methods="GET")
+     */
+    public function edit()
+    {
+        return $this->render('admin/article/index.html.twig', [
+            'articles' => $this->articleRepository->findAllArticle()
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin.article.delete", methods="DELETE")
+     */
+     public function delete(Article $article, Request $request)
+     {
+         if($this->isCsrfTokenValid('delete' . $article->getId(), $request->get('_token')))
+         {
+           $this->em->remove($article);
+           $this->em->flush();
+           $this->addFlash('success', 'Supprimé avec succès');
+         }
+         return $this->redirectToRoute('admin.article.index');
+     }
 }
